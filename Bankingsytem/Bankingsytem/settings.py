63f8b1fe76447a333430bc9cf793e01a15f7eb81
Hema <hemapaun07@gmail.com>
2021-08-16 16:42:56 +0530

    first commit

--- Bankingsytem/Bankingsytem/settings.py
+++ Bankingsytem/Bankingsytem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u5fi@u0w^st2^3#(9ijst)apikqf08)wpg4a5^2lblk+wpd3wt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tbf-bnking.herokuapp.com']
 
 
 # Application definition
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'customer.apps.CustomerConfig',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,5 +132,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
